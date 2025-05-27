a437e40cb46640e37b760e6194849333d68b4aee
Cézar Maldini <cezar.datascience@gmail.com>
2025-05-27 11:16:53 -0300

    Configuração final do env

--- config/db.py
+++ config/db.py
@@ -9,13 +9,6 @@ load_dotenv()
 # Cache para evitar reconexões em cada reload
 @st.cache_resource
 def get_engine():
-    user = "postgres.kjmflmvmlayjmdhzqxbz"
-    password = "Z9OEnfDEnYxGpqEt"
-    host = "aws-0-sa-east-1.pooler.supabase.com"
-    port = "6543"
-    database = "postgres"
-
-    url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
     db_url = os.getenv('DB_URL')
     engine = create_engine(db_url)
     return engine
