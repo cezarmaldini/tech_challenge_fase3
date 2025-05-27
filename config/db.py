ba73854dbe7cd8289b506a78e0398d964a2b1fa4
Cézar Maldini <cezar.datascience@gmail.com>
2025-05-27 11:15:43 -0300

    Configuração do env

--- config/db.py
+++ config/db.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import pandas as pd
 import streamlit as st
+import os
+
+load_dotenv()
 
 # Cache para evitar reconexões em cada reload
 @st.cache_resource
@@ -12,5 +16,6 @@ def get_engine():
     database = "postgres"
 
     url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
-    engine = create_engine(url)
+    db_url = os.getenv('DB_URL')
+    engine = create_engine(db_url)
     return engine
